80a572305730c946a7d04437549241e7abf5fdbb
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2025-01-20 20:03:42 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,9 +7,9 @@ terraform {
   }
  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "vgsterraform"
+    bucket = "ajithvgs"
     key    = "vgs/terraform.tfstate"
-    region = "ap-south-1"   
+    region = "us-east-1"   
   }
 }
 
