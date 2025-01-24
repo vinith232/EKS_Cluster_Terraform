7fb098673ef8b59c9bbe63a9fbb100ce9e2dea69
VenkatVGS <159562946+VenkatVGS@users.noreply.github.com>
2025-01-24 11:46:59 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,7 +19,8 @@ provider "aws" {
 
 locals {
   region = "us-east-1"
-  name   = "amonkincloud-cluster"
+  name = "vgs_cluster"
+  #name   = "amonkincloud-cluster"
   vpc_cidr = "10.123.0.0/16"
   azs      = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
