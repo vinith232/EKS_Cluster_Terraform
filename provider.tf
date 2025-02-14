6995ea63783235c52aa9160c302962097218f133
vinith232 <92229301+vinith232@users.noreply.github.com>
2025-02-15 00:31:30 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,19 +9,19 @@ terraform {
   backend "s3" {
     bucket = "vinith-14-02-2025"
     key    = "vgs/terraform.tfstate"
-    region = "us-east-1"   
+    region = "us-west-1"   
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-1"
 }
 
 locals {
-  region = "us-east-1"
+  region = "us-west-1"
   name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["us-west-1a", "us-west-1b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
