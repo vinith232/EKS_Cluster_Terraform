edf60b3f6f93498116743019326550b4c5320afd
vinith232 <92229301+vinith232@users.noreply.github.com>
2025-02-15 00:48:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -21,7 +21,7 @@ locals {
   region = "us-west-1"
   name = "vgs_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-west-1a", "us-west-1b"]
+  azs      = ["us-west-1a", "us-west-1c"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
